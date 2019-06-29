f75470d9009a7e0eda4c22127c7895f61b647186
Andreas Möller <am@localheinz.com>
2019-06-29 20:46:11 +0200

    Enhancement: Aggregate HTTP methods related to WebDAV

---
+++ src/Method/WebDav.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2019 Andreas Möller
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ *
+ * @see https://github.com/localheinz/http-method
+ */
+
+namespace Localheinz\Http\Method;
+
+/**
+ * @see http://www.webdav.org
+ */
+interface WebDav extends
+    Rfc\Rfc3648,
+    Rfc\Rfc3744,
+    Rfc\Rfc4437,
+    Rfc\Rfc4791,
+    Rfc\Rfc4918,
+    Rfc\Rfc5323,
+    Rfc\Rfc5789,
+    Rfc\Rfc5842,
+    Rfc\Rfc7231
+{
+}
